b646e770ff7b6795ec1a2bb6a89aeb4b5c531296
klannie <klannieha@gmail.com>
2019-09-30 16:34:38 -0400

    added params

--- openswath/run_osw.sh
+++ openswath/run_osw.sh
@@ -3,7 +3,8 @@
 basefolder=/home/klannie/Desktop/dia_pipeline
 src=$basefolder/src
 data=$basefolder/data/DIA/mzML
-tr_lib=$basefolder/data/libraries/Ankit_HEK_qc_targetdecoy_assaylib.pqp
+tr_lib=$basefolder/data/libraries/HEK_qc_mqout_SCX_10only_assaylib_targetdecoy.pqp
+#tr_lib=$basefolder/data/libraries/Ankit_HEK_qc_targetdecoy_assaylib.pqp
 irt=$basefolder/data/libraries/irt/iRTassays.TraML
 dir=`pwd`
 input=$(find $data -maxdepth 1 -type f -name "*.mzML")
@@ -13,7 +14,8 @@ for f in $input;do
 	filename="${filename%.*}"
 	log=${filename}.log
 	#script $log
-	. $src/run_osw_Orbitrap_HEK.sh $f $tr_lib $irt $dir/$filename False 4 klannie
+	. $src/run_osw_Orbitrap_HEK.sh $f $tr_lib $irt $dir/$filename False 4 klannie \
+		"-RTNormalization:alignmentMethod linear -RTNormalization:outlierMethod none -Scoring:TransitionGroupPicker:compute_peak_quality false -ms1_isotopes 3"
         mv debug_ppmdiff.txt ${filename}_debug_ppmdiff.txt	
 	sleep 1s
 	#exit
