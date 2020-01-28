ff63550a5575b353382ac8acb731d3862d38c88a
Annie Ha <annieha@gra-login1.graham.sharcnet>
2020-01-28 17:21:52 -0500

    Updated stats function

--- analysis/Utilities.R
+++ analysis/Utilities.R
@@ -40,10 +40,52 @@ AllPeptides_filter <- function(Peptidesdf){
 #}
 
 
+CommonPeptides <- function(msms_lst){
+  # list of dataframe from multiple runs
+  runs <- 1:length(msms_lst)
+  lst <- lapply(runs, function(x){
+    run <- msms_lst[[x]]
+    run <- run[order(PEP)]
+    sequence <- run['Modified sequence']
+    sequence <- unique(sequence)
+    sequence <- reformat_mods(sequence)
+    sequence})
+  common <- Reduce(intersect, lst)
+  return(common)
+}
+  
 
 
+# Need a function to merge list of dataframes and get mean or sd or na
 
 
-
+CommonPeptideStats <- function(msms_lst, run_ids, column,  mean = T, sd = T, na = T){
+  "
+  msms_lst	list of dataframes from msms files first column contains peptide sequence
+  run_ids 	list of run ids to replace for column names
+  column	string of the column that is interested in analyzing e.g. RT or Intensity
+  "
+  df <- lapply(1:length(run_id), function(x){
+    rep <- subset(msms_lst[[x]], select=c("PeptideSequence", column))
+    setnames(rep, column, run_ids[x])
+    rep})
+  df <- Reduce(function(x, y) merge(x,y, by = "PeptideSequence", all=TRUE, df))
+  if(mean){
+    mean <- lapply(1:nrow(df), function(x){
+      m <- df[x, 2:ncol(df)] %>% as.numeric() %>% mean(na.rm=T)
+      m}) %>% unlist()
+    df$mean <- mean
+  }else if(sd){
+    sd <- lapply(1:nrow(df), function(x){
+      s <- df[x, 2:ncol(df)] %>% as.numeric() %>% sd(na.rm=T)
+      s}) %>% unlist()
+    df$sd <- sd
+  }else if(na){
+    na <- lapply(1:nrow(df), function(x){
+      n <- df[x, 2:ncol(df)] %>% is.na() %>% sum()
+      n}) %>% unlist()
+    df$na <- na}
+  return(df)
+}
 
 
