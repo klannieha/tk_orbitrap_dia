4abb2e729232874ac78866864113e7e76ce6097a
Annie Ha <klannieha@gmail.com>
2022-08-05 17:44:44 -0400

    finished loop

--- convertXICtosqMass.py
+++ convertXICtosqMass.py
@@ -13,8 +13,9 @@ from pyopenms import *
 # pseudo-code:
 # read diann XIC data only take 1 file at a time (no combined file)
 diann = sys.argv[1]
+filename = sys.argv[2]
 xics = pd.read_csv(diann, sep = "\t")
-
+xics = xics.reset_index(drop = True)
 # rename all the column names
 xics = xics.rename(columns = {"Precursor.Id": "PrecursorId", "File.Name": "filename", "Retention.Times": "RetentionTimes", "Theoretical.Mz":"TheoreticalMz", "MS.Level": "MSLevel", "Modified.Sequence":"ModifiedSequence", "Stripped.Sequence":"Sequence"})
 
@@ -23,20 +24,18 @@ print("Number of files detected:", len(runs))
 
 # separate xics by file
 xics = xics.loc[xics.filename == runs[0]] 
+# data columns start from file1.columns[14:600]
 
-
-# get values from each row
-
-precursors = xics.PrecursorId.unique()
-
+# annotate Fragment ions/transitions
+xics = xics.astype({"FragmentSeriesNumber":'str'})
+conditions = [xics["MSLevel"] == 1, xics["MSLevel"] == 2]
+values = [xics["PrecursorId"] + "_Prec", xics["PrecursorId"] + "_" + xics['FragmentSeriesNumber']+xics['FragmentType']]
+xics['FragmentAnnotation'] = np.select(conditions, values)
 
 # store as mzML
 exp = MSExperiment()
 
 # use pyopenms MSChromatogram()
-chromatogram = MSChromatogram()
-
-# set precursor
 # assign peaks
 # chromatogram.set_peaks([rt,i])
 
@@ -47,7 +46,42 @@ chromatogram = MSChromatogram()
 
 # store as mzML in the end
 #exp = MSExperiment()
-exp.addChromatogram(chromatogram)
+
+
+# get values from each row
+
+precursors = xics.PrecursorId.unique()
+transitions = xics.FragmentAnnotation.unique()
+
+trRT = xics.loc[xics.RetentionTimes == 1]
+trInt = xics.loc[xics.Intensities == 1]
+trID = xics[["filename", "TheoreticalMz", "MSLevel", "ModifiedSequence", "Sequence", "PrecursorId", "FragmentType", "FragmentSeriesNumber", "FragmentAnnotation", "FragmentCharge"]].drop_duplicates()
+
+# start loop
+
+for i in range(0, len(precursors)):
+    p = precursors[i]
+    tr = trID.loc[trID.PrecursorId == p]
+    tr = tr.reset_index(drop = True)
+    tr = tr.FragmentAnnotation
+    Prec = Precursor()
+    Prec.setMZ(trID.loc[(trID.PrecursorId == p) & (trID.MSLevel == 1), "TheoreticalMz"].values[0])
+    Prec.setMetaValue("peptide_sequence", trID.loc[trID.PrecursorId == p].ModifiedSequence.unique()[0])
+    for j in range(0, len(tr)):
+        tID = tr[j]
+        chrom = MSChromatogram()
+        rt = trRT.loc[trRT.FragmentAnnotation == tID]
+        rt = rt.iloc[:, 14:615].values[0]
+        Int = trInt.loc[trInt.FragmentAnnotation == tID]
+        Int = Int.iloc[:, 14:615].values[0]
+        chrom.set_peaks([rt, Int])
+        chrom.setNativeID(tID)
+        prod = Product()
+        prod.setMZ(trID.loc[trID.FragmentAnnotation == tID].TheoreticalMz.unique()[0])
+        chrom.setProduct(prod)
+        chrom.setPrecursor(Prec)
+        exp.addChromatogram(chrom)
+    
 
 # end loop
 # save file
